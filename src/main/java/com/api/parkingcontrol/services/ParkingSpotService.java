3bf4f82e8f6b2725634461e9bd962859bcfb4b6d
thmedeiroslima <97640552+thmedeiroslima@users.noreply.github.com>
2022-08-09 09:10:48 -0300

    Implementação Classes ParkingSpotController, ParkingSpotDto e ParkingSpotService

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,9 +1,12 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -20,4 +23,8 @@ public class ParkingSpotService {
 
     }
 
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
 }
